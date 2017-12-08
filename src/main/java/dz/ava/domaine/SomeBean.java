513b182805f53af5dd735cf47074210b1fa8defc
Farid FAOUDI <linuxfarid@gmail.com>
2017-12-08 11:40:59 +0100

    Dynamic Filtering

--- src/main/java/dz/ava/domaine/SomeBean.java
+++ src/main/java/dz/ava/domaine/SomeBean.java
@@ -1,15 +1,17 @@
 package dz.ava.domaine;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
     private String field2;
 
-    @JsonIgnore
+    //@JsonIgnore
     private String field3;
 }
--- src/main/java/dz/ava/resources/FilteringResource.java
+++ src/main/java/dz/ava/resources/FilteringResource.java
@@ -1,7 +1,11 @@
 package dz.ava.resources;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import dz.ava.domaine.SomeBean;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,33 @@ public class FilteringResource {
     @GetMapping("/filtering-list")
     public List<SomeBean> retrieveListOfSomeBean(){
         return Arrays.asList(new SomeBean("value1","value2","value3"), new SomeBean("value11","value22","value33"));
+    }
+
+    @GetMapping("/filtering-dynamic")
+    public MappingJacksonValue dynamicFiltering(){
+        SomeBean someBean = new SomeBean("value1","value2","value3");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field2");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/filtering-list-dynamic")
+    public MappingJacksonValue dynamicFilteringListOfSomeBean(){
+        List<SomeBean> someBeans = Arrays.asList(new SomeBean("value1","value2","value3"), new SomeBean("value11","value22","value33"));
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2","field3");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(someBeans);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 }
