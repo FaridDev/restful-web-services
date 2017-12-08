abf316773019506e6931317b4f3a6902986c9ebb
Farid FAOUDI <linuxfarid@gmail.com>
2017-12-08 10:29:25 +0100

    Enhancing Swagger Documentation with custom annotations

--- src/main/java/dz/ava/domaine/User.java
+++ src/main/java/dz/ava/domaine/User.java
@@ -1,5 +1,7 @@
 package dz.ava.domaine;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,48 +10,18 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
-//@Data
-//@AllArgsConstructor
+@ApiModel(description = "All details about the user")
+@Data
+@AllArgsConstructor
 public class User implements Serializable{
 
     private Integer id;
 
+    @ApiModelProperty("Name should have at least 2 characters")
     @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
 
+    @ApiModelProperty(notes = "Birth date should be in the past")
     @Past
     private Date birthDate;
-
-    public User() {
-    }
-
-    public User(Integer id, @Size(min = 2, message = "Name should have at least 2 characters") String name, @Past Date birthDate) {
-        this.id = id;
-        this.name = name;
-        this.birthDate = birthDate;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
 }
